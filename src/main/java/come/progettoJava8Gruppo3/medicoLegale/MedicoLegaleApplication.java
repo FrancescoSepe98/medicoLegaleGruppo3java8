3b45c3f70fc690f5519602cc530afb52449aebcb
FrancescoSepe98 <sepe.francesco.lavoro@gmail.com>
2023-03-02 10:44:53 +0100

    First commit

---
+++ src/main/java/come/progettoJava8Gruppo3/medicoLegale/MedicoLegaleApplication.java
@@ -0,0 +1,13 @@
+package come.progettoJava8Gruppo3.medicoLegale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicoLegaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicoLegaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/come/progettoJava8Gruppo3/medicoLegale/MedicoLegaleApplicationTests.java
@@ -0,0 +1,13 @@
+package come.progettoJava8Gruppo3.medicoLegale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicoLegaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
